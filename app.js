5d8010baa7ce686b0f36a077eb6b9377da0c419e
SAADHUSSAIN31821 <saadhussain31821@gmail.com>
2023-10-15 14:28:26 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,112 @@
+// // chapter# 01
+
+// // Question # 01
+// alert("Hello")
+
+// // Question # 02 
+// alert("Error! Please enter a valid password")
+
+// // Question # 03 
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// // Question # 04
+// alert("Welcome to JS Land...")
+// alert(" Happy Coding!")
+
+// // Question # 05
+// console.log("Hello...I can run JS through My web browser's console")
+
+// // Chapter # 02
+// // Question # 01 
+
+// var userName = (232)
+
+// // Question # 02
+// var myName = "Muhammad Saad Husaain";
+
+// // Question # 03
+// var massage 
+// var massage = "Hello World";
+// alert(massage)
+
+// // Question # 04
+// var name = "Muhammad Saad Husaain";
+// alert(name)
+
+// var birthDay = "18 Year old";
+// alert(birthDay)
+
+// var about = "I am frontend web developer..";
+// alert(about)
+
+
+// // Question # 05
+// alert("pizza\n  pizz\n  piz\n  pi\n  p")
+
+// // Question # 06
+// var email = "My email address is saadhussain31821@gmail.com";
+// alert(email)
+
+// // Question # 07
+// var book = "A smarter way to learn JavaScript.";
+// alert(book)
+
+// // Question # 09
+// var pattren = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(pattren )
+
+// // chapter # 3
+// // Question # 01
+ 
+// var age = " i am"
+// var a = 18
+// var year = " year old"
+// alert(age + a +  year)
+
+// // Question # 01
+
+// var visitor = " You have visited this site "
+// var num = 14
+// var time = " times"
+// alert(visitor + num + time)
+
+// // chapter # 4
+// // Question # 01
+
+// var variable1 = "Hello world", variable2 = "testing"; variable3 = "result"
+
+// // Question # 02
+
+// // 3 illegal variable name 
+
+// // cannot start with Number
+// // var 1veriable="world";
+
+// //  cannot end with underscore
+// // var time_ ="hellow"
+
+// // cannot use addition sign
+// // var food+nonfood = "smit"
+
+// // 3  legal variable name 
+
+// // start with letter 
+// var first ="md";
+// var last ="sharyar"
+
+// // start with underscore
+// var _varible="Myname"
+
+// // start with doller
+// var $legal =" veriable"
+
+// var my_1stname ="md"
+
+var num = prompt("It is very esay \n press any number");
+// 1+10
+if(num===""){
+    alert("error")
+}
+else{
+    alert(num)
+}
